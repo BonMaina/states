a20718c75c75d70ef206d754e679fa836b78f1cb
BonMaina <bonifacemaina2012@gmail.com>
2024-04-30 11:43:53 +0300

    Initial commit with updated project

--- index.js
+++ index.js
@@ -2,54 +2,45 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs'); // require the file system module
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions'); 
 const PORT = process.env.PORT || 3500;
 const mongoose = require('mongoose');
-const onnectDB = require('./config/database');
-const { connect } = require('http2');
 const connectDB = require('./config/database');
-const credentials = require('./middleware/credentials');
 
 connectDB();
 
-app.use(credentials);
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
 
 // Body parser middleware to handle JSON data
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+//     next();
+// });
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', require('./routes/root'));
 
 app.use('/states', require('./routes/states'));
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.use('/', require('./routes/root'));
-
-app.get('^/$|states(.json)?', (req, res) => {
-    // res.sendFile('./views/index.html', { root:_});
-    res.sendFile(path.join(__dirname, 'data', 'statesData.json'));
-})
-app.get('/^/$|new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
-app.get('/^/$|old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html');
-});
-
-
-
-
-app.get('/*',(req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
 
 mongoose.connection.once('open', () => {
